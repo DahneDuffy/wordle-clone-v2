72cfd390ad253fb5faf3e3b633c066d3bda9bccc
Dahne Duffy <55709717+DahneDuffy@users.noreply.github.com>
2022-06-30 12:09:18 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 3000;
+const axios = require("axios").default;
 const express =require('express')
+const cors = require('cors')
 const fs = require('fs')
 require('dotenv').config()
-const path = require('path')
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'))
-
-
+app.use(cors())
+app.use(express.static('/'))
 
 app.get('/',(req,res)=>{
-  res.sendFile(path.join((__dirname +'/public/index.html')))
+  res.send('Server Running...')
 })
 
+app.get('/word',(req, res)=>{
+    const options = {
+        method: 'GET',
+        url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
+        params: {count: '5', wordLength: '5'},
+        headers: {
+          'X-RapidAPI-Host': 'random-words5.p.rapidapi.com',
+          'X-RapidAPI-Key': process.env.API_KEY
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+          res.json(response.data[0])
+      }).catch(function (error) {
+          console.error(error);
+      });
+})
+
+app.get('/check', (req,res)=>{
+    const word = req.query.word;
+
+    const options = {
+        method: 'GET',
+        url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/association/',
+        params: {entry: word},
+        headers: {
+          'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com',
+          'X-RapidAPI-Key': process.env.API_KEY
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+          res.json(response.data.result_msg)
+      }).catch(function (error) {
+          console.error(error);
+      });
+})
 
 
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
